80be31ef5ad7317278ec0de5a32883f79505bd8f
bierbaumtim <tb2706@outlook.de>
2024-02-11 21:10:47 +0100

    add first version of log viewer

--- yalp_flutter/lib/views/material_log_viewer.dart
+++ yalp_flutter/lib/views/material_log_viewer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:date_format/date_format.dart';
 import 'package:yalp_core/yalp_core.dart';
 
 class MaterialLogViewer extends StatefulWidget {
@@ -22,16 +24,15 @@ class _MaterialLogViewerState extends State<MaterialLogViewer> {
           if (snapshot.hasData) {
             final logs = snapshot.data as List<LogEntry>;
 
+            if (logs.isEmpty) {
+              return const Center(
+                child: Text('No logs found'),
+              );
+            }
+
             return ListView.builder(
               itemCount: logs.length,
-              itemBuilder: (context, index) {
-                final log = logs[index];
-
-                return ListTile(
-                  title: Text(log.message),
-                  subtitle: Text(log.timestamp.toIso8601String()),
-                );
-              },
+              itemBuilder: (context, index) => _LogEntryCard(log: logs[index]),
             );
           } else if (snapshot.hasError) {
             return Center(
@@ -43,6 +44,126 @@ class _MaterialLogViewerState extends State<MaterialLogViewer> {
             );
           }
         },
+      ),
+    );
+  }
+}
+
+class _LogEntryCard extends StatelessWidget {
+  const _LogEntryCard({
+    super.key,
+    required this.log,
+  });
+
+  final LogEntry log;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: () {},
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ListTile(
+              title: Text(log.message),
+              subtitle: Text(
+                formatDate(
+                  log.timestamp,
+                  [
+                    ...[dd, '.', mm, '.', yyyy],
+                    ' - ',
+                    ...[HH, ':', nn, ':', ss, '.', SSS]
+                  ],
+                ),
+              ),
+              trailing: _Tag(
+                color: switch (log.level) {
+                  LogLevel.trace => Colors.grey,
+                  LogLevel.debug => Colors.blue,
+                  LogLevel.info => Colors.green,
+                  LogLevel.warning => Colors.orange,
+                  LogLevel.error => Colors.red,
+                  LogLevel.fatal => Colors.purple,
+                },
+                value: log.level.name,
+              ),
+              mouseCursor: SystemMouseCursors.click,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+              child: Wrap(
+                spacing: 8,
+                runSpacing: 8,
+                children: [
+                  if (log.tag case var tag?) _Tag(value: tag),
+                  _Tag(
+                    value: '${log.className}.${log.functionName}',
+                    color: Colors.lightBlueAccent,
+                  ),
+                  if (log.invocation case var invocation?)
+                    _Tag(value: invocation, color: Colors.pinkAccent),
+                ],
+              ),
+            ),
+            if (log.error case final error?)
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: Text(
+                  '$error',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontFamily: 'monospace',
+                      ),
+                ),
+              ),
+            if (log.stackTrace case final stacktrace?
+                when (log.level == LogLevel.error ||
+                    log.level == LogLevel.fatal ||
+                    log.level == LogLevel.warning))
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: Text(
+                  '$stacktrace',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontFamily: 'monospace',
+                      ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Tag extends StatelessWidget {
+  final String value;
+  final Color color;
+
+  const _Tag({
+    super.key,
+    required this.value,
+    this.color = Colors.blueAccent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 4,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        value,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
       ),
     );
   }
